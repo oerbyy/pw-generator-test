2714d294f630a1ed6762c928b572cb803eca44b4
oerbyy <oerbyy@gmail.com>
2023-09-25 17:03:41 +0200

    Feat: add Password Generator component; update styles;

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import './App.css';
+import PasswordGenerator from './components/PasswordGenerator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <h1 className="title">Oerbyy's Password Generator</h1>
+      </div>
+
+      <div className="content">
+        <PasswordGenerator />
+      </div>
+
+      <div className="footer">&copy; 2023 Volodymyr Shchukin</div>
     </div>
   );
 }
---
+++ src/components/PasswordGenerator.tsx
@@ -0,0 +1,99 @@
+import React, {useState, useEffect} from 'react';
+import './PasswordGenerator.css';
+
+import {generatePassword} from '../helpers/commonHelper';
+
+function PasswordGenerator() {
+  const [password, setPassword] = useState<string>('');
+  const [length, setLength] = useState<number>(12);
+  const [includeLowercase, setIncludeLowercase] = useState<boolean>(true);
+  const [includeUppercase, setIncludeUppercase] = useState<boolean>(false);
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+
+  const handleCopyClick = () => {
+    if (password) {
+      navigator.clipboard.writeText(password);
+      alert('Password copied to clipboard!');
+    }
+  };
+
+  const handleGeneratePassword = () => {
+    const newPassword = generatePassword(
+      {includeLowercase, includeUppercase, includeNumbers, includeSymbols},
+      length
+    );
+    setPassword(newPassword);
+  };
+
+  return (
+    <div className="centered-container">
+      <div className="password-generator">
+        <div className="h-centered">
+          <h1>Password Generator</h1>
+        </div>
+        <input type="text" value={password} readOnly />
+
+        <button onClick={handleCopyClick}>Copy</button>
+
+        <div className="password-options">
+          <label>Password Length: {length}</label>
+          <input
+            type="range"
+            min={4}
+            max={24}
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+          />
+        </div>
+        <div className="password-options">
+          <label>
+            <input
+              type="checkbox"
+              checked={includeLowercase}
+              onChange={() => setIncludeLowercase(!includeLowercase)}
+            />
+            Include Lowercase
+          </label>
+        </div>
+        <div className="password-options">
+          <label>
+            <input
+              type="checkbox"
+              checked={includeUppercase}
+              onChange={() => setIncludeUppercase(!includeUppercase)}
+            />
+            Include Uppercase
+          </label>
+        </div>
+        <div className="password-options">
+          <label>
+            <input
+              type="checkbox"
+              checked={includeNumbers}
+              onChange={() => setIncludeNumbers(!includeNumbers)}
+            />
+            Include Numbers
+          </label>
+        </div>
+        <div className="password-options">
+          <label>
+            <input
+              type="checkbox"
+              checked={includeSymbols}
+              onChange={() => setIncludeSymbols(!includeSymbols)}
+            />
+            Include Symbols
+          </label>
+        </div>
+
+        <br />
+        <div className="h-centered">
+          <button onClick={handleGeneratePassword}>GENERATE PASSWORD</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PasswordGenerator;
