35c95cc09fecb3a26277a19e2e20d9640167a579
oerbyy <oerbyy@gmail.com>
2023-09-25 16:07:48 +0200

    Feat: add constants;

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const UPPER_CASE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const NUMBER_CHARS = '0123456789';
+export const SYMBOL_CHARS = '!@#$%^&*()_+[]{}|;:,.<>?';
+export const LOWER_CASE_CHARS = 'abcdefghijklmnopqrstuvwxyz';
